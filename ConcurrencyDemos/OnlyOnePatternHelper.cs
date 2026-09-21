443e757713cdf2798fe101183b8ce7959a515bbf
Armen <armen@legaldesk.dk>
2026-09-21 12:55:26 +0400

    OnlyOnePattrn

---
+++ ConcurrencyDemos/OnlyOnePatternHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace ConcurrencyDemos
+{
+    public class OnlyOnePatternHelper
+    {
+        public static async Task<T> RunAsync<T>(IEnumerable<Func<CancellationToken, Task<T>>> funcs)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var tasks = funcs.Select(func => func(cancellationTokenSource.Token));
+            var task = await Task.WhenAny(tasks);
+            cancellationTokenSource.Cancel();
+            return await task;
+        }
+
+
+        public static async Task<T> RunAsync<T>(params Func<CancellationToken, Task<T>>[] funcs)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var tasks = funcs.Select(func => func(cancellationTokenSource.Token));
+            var task = await Task.WhenAny(tasks);
+            cancellationTokenSource.Cancel();
+            return await task;
+        }
+
+
+        public static async Task<string> GetContent(string name, CancellationToken cancellationToken)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync($"https://httpbin.org/get?foo={name}", cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
--- ConcurrencyDemos/Program.cs
+++ ConcurrencyDemos/Program.cs
@@ -1,13 +1,36 @@
-﻿namespace ConcurrencyDemos
+﻿using System.Xml.Linq;
+
+namespace ConcurrencyDemos
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            FireAndForgetHelper.Run(FireAndForgetHelper.FireAndForget);
 
+            //FireAndForgetHelper.Run(FireAndForgetHelper.FireAndForget);
+
+            await OnlyOnePattrn();
 
             Console.ReadLine();
+        }
+
+        private static async Task OnlyOnePattrn()
+        {
+            var names = new string[] { "Jack", "Jane", "Joe" };
+            var tasks = names.Select(name =>
+            {
+                Func<CancellationToken, Task<string>> func = (ct) => OnlyOnePatternHelper.GetContent(name, ct);
+                return func;
+            });
+
+            // option 2
+            //var tasks = names.Select(name => new Func<CancellationToken, Task<string>>((cancellationToken) =>
+            //{
+            //    return OnlyOnePatternHelper.GetContent(name, cancellationToken);
+            //}));
+
+            var content = await OnlyOnePatternHelper.RunAsync(tasks);
+            Console.WriteLine(content);
         }
     }
 }
